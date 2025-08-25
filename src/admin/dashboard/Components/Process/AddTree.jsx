c5ef3bcf315d063f0986e0aab5fbe61a4536b57e
Parbat Nil Bera <parbatnilbera743@gmail.com>
2025-08-25 11:46:37 +0530

    fix

--- src/admin/dashboard/Components/Process/AddTree.jsx
+++ src/admin/dashboard/Components/Process/AddTree.jsx
@@ -1,7 +1,47 @@
+// import React, { useState } from "react";
+// import axiosInstance from "../../../../utils/axiosInstance";
+// import AddTreeModal from "../Modal/AddTreeModal";
+// import { FiEdit, FiTrash, FiPlus, FiGrid, FiList } from "react-icons/fi";
+// import { API_PATH } from "../../../../utils/apiPath";
+
+// const TreeTable = ({ setPlants }) => {
+//   const [showModal, setShowModal] = useState(false);
+
+//   const refreshPlants = (newPlant) => {
+//     console.log(newPlant);
+//     setPlants((prevPlants) => [newPlant, ...prevPlants]);
+//   };
+//   const handleAddTree = async (data) => {
+//     try {
+//       // Example API call
+//       const res = await axiosInstance.post(API_PATH.PLANT.ADD_PLANT, data, {
+//         headers: { "Content-Type": "multipart/form-data" },
+//       });
+//       console.log("Added:", res.data);
+//     } catch (error) {
+//       console.error("Error adding tree:", error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       {/* Modal */}
+//       <AddTreeModal
+//         refreshPlants={refreshPlants}
+//         isOpen={showModal}
+//         setPlants={setPlants}
+//         onClose={() => setShowModal(false)}
+//         onSubmit={handleAddTree}
+//       />
+//     </div>
+//   );
+// };
+
+// export default TreeTable;
+
 import React, { useState } from "react";
 import axiosInstance from "../../../../utils/axiosInstance";
 import AddTreeModal from "../Modal/AddTreeModal";
-import { FiEdit, FiTrash, FiPlus, FiGrid, FiList } from "react-icons/fi";
 import { API_PATH } from "../../../../utils/apiPath";
 
 const TreeTable = ({ setPlants }) => {
@@ -11,9 +51,9 @@ const TreeTable = ({ setPlants }) => {
     console.log(newPlant);
     setPlants((prevPlants) => [newPlant, ...prevPlants]);
   };
+
   const handleAddTree = async (data) => {
     try {
-      // Example API call
       const res = await axiosInstance.post(API_PATH.PLANT.ADD_PLANT, data, {
         headers: { "Content-Type": "multipart/form-data" },
       });
@@ -24,7 +64,9 @@ const TreeTable = ({ setPlants }) => {
   };
 
   return (
-    <div>
+    <div className="w-full">
+      {/* Button to open modal */}
+
       {/* Modal */}
       <AddTreeModal
         refreshPlants={refreshPlants}
@@ -32,6 +74,7 @@ const TreeTable = ({ setPlants }) => {
         setPlants={setPlants}
         onClose={() => setShowModal(false)}
         onSubmit={handleAddTree}
+        className="w-full max-w-lg mx-auto sm:mx-auto sm:w-full sm:h-auto"
       />
     </div>
   );
